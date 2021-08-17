67e4364e40f5d1b2733178de0dabf60f262b4cd3
Vhudi <vhudimotabatsindi@gmail.com>
2021-08-17 13:14:37 +0200

    task 0.6

---
+++ task-0.6/task-0.6.js
@@ -0,0 +1,10 @@
+{
+  function findMaxValue(...args) {
+    // ...args is the native object that represents function arguments.
+    var max = args[0];
+    for (var i = 1; i < args.length; i++) {
+      if (max < args[i]) max = args[i];
+    }
+    return max;
+  }
+}
