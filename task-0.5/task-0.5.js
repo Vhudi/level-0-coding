f936c7a2b55bc2898f77c37bf52d53faaed4f114
Vhudi <vhudimotabatsindi@gmail.com>
2021-08-17 12:41:35 +0200

    task 0.5

---
+++ task-0.5/task-0.5.js
@@ -0,0 +1,8 @@
+{
+  function calculateArea(length, height, width) {
+    var semiP; // semiperimiter of triangle
+    var area;
+    semiP = (length + height + width) / 2;
+    return semiP;
+  }
+}
