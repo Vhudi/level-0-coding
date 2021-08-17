959a4770fd357ca1528a44a7ff07a30758436c83
Vhudi <vhudimotabatsindi@gmail.com>
2021-08-17 12:26:02 +0200

    task 0.3 and 0.4

---
+++ task-0.3/task-0.3.js
@@ -0,0 +1,7 @@
+{
+  function hello(name) {
+    console.log("Hello " + name + "!");
+  }
+
+  hello("Tshepo"); // an example of the required input.
+}
---
+++ task-0.4/task-0.4.js
@@ -0,0 +1,12 @@
+{
+  function evenOrOdd(num) {
+    if (num % 2 == 1) console.log("odd");
+    else console.log("even");
+  }
+
+  //Testing code
+  // console.log("e.g 3 is ");
+  // evenOrOdd(3);
+  // console.log("e.g 4 is ");
+  // evenOrOdd(4);
+}
