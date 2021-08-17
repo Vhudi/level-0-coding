cd64742d3852d6912aecd762b4ba55bccf55a0ca
Vhudi <vhudimotabatsindi@gmail.com>
2021-08-17 13:25:15 +0200

    task 0.7

---
+++ task-0.7/task-0.7.js
@@ -0,0 +1,11 @@
+{
+  function celsiusToFahrenheit(num) {
+    var answer = num * (9 / 5) + 32;
+    return answer;
+  }
+
+  function fahrenheitToCelsius(num) {
+    var answer = (num - 32) * (5 / 9);
+    return answer;
+  }
+}
